a5185b4940341723eff923bdb9bebf1f8d06a9e6
FrancoLiberali <franco.liberali@gmail.com>
2019-04-28 17:20:26 -0300

    compatile con assembly

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # orgatp1
+
+compilar normal: gcc -Wall -g -o tp.exe ant_engine.c artist_ant.c
+compilar con assembly: gcc -Wall -g -D ASSEMBLY -o tp.exe paint.S ant_engine.c artist_ant.c
--- ant_engine.c
+++ ant_engine.c
@@ -15,6 +15,7 @@ palette_size(colour_t *array)
 	return i;
 }
 
+#ifndef ASSEMBLY
 void *
 paint(void *ant_p, void *grid_p, void *palette_p, void *rules_p, uint32_t iterations)
 {
@@ -75,6 +76,7 @@ paint(void *ant_p, void *grid_p, void *palette_p, void *rules_p, uint32_t iterat
 	}
 	return grid;
 }
+#endif
 
 void *
 make_rules(char *spec)
