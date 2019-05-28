cf4981143e40c3a6fe2738650eff715e292214d6
edjus <justo.edson@gmail.com>
2019-05-27 21:18:22 -0300

    Agrego memoria principal mapeado en la cache y algnas funciones

--- cache.c
+++ cache.c
@@ -1,10 +1,12 @@
 #include "cache.h"
+#include <stdlib.h>
 
 void free_cache(cache_t *cache, int iterations){
 	for (;iterations > 0; iterations--){
 		free(cache -> vias[iterations-1]);
 	}
 	free(cache -> vias);
+	free(cache->memory);
 }
 
 void free_colas(cache_t *cache, size_t iterations){
@@ -16,9 +18,17 @@ void free_colas(cache_t *cache, size_t iterations){
 }
 
 bool init_cache(cache_t *cache){
-	cache -> vias = malloc(N_VIAS*(sizeof(block_t*)));
+	cache-> vias = malloc(N_VIAS*(sizeof(block_t*)));
 	if (!cache -> vias) return false;
 	
+	cache->memory = (unsigned char*) malloc(MEMORY_SIZE);
+	if (!(cache->memory)){
+		free_cache(cache, 0);
+		return false;
+	}
+
+    memset(cache->memory,0 ,MEMORY_SIZE);
+
 	for (int i = 0; i < N_VIAS; i++){
 		(cache -> vias)[i] = malloc(N_BLOCKS*(sizeof(block_t)));
 		if (!(cache -> vias)[i]){
@@ -59,4 +69,12 @@ unsigned int get_index (unsigned int address){
 }
 unsigned int get_offset (unsigned int address){
 	return (address & OFFSET);
+}
+
+unsigned int find_set(unsigned int address){
+	return get_index(address);
+}
+
+float get_miss_rate(){
+	return (((float)cache->n_miss / (float)cache->n_acces) * 100);
 }
--- cache.h
+++ cache.h
@@ -8,32 +8,42 @@
 
 #define N_VIAS 4
 #define N_BLOCKS 8
+#define MEMORY_SIZE 65536 //en bytes
 #define TAG 0xFE00
 #define INDEX 0x1C0
 #define OFFSET 0x3F
 
 typedef struct {
     block_t **vias;
-    cola_t **fifos;
+    cola_t **fifos; //ver si va, según la consigna se debe deducir con los metadatos
+
+	unsigned char* memory; //pongo acá la memoria principal para facilitar acceso
+
     unsigned int n_acces;
     unsigned int n_miss;
 } cache_t;
 
+cache_t* cache; //cache como variable global
 
 bool init_cache(cache_t *cache);
 void delete_cache(cache_t *cache);
 unsigned int get_tag (unsigned int address);
 unsigned int get_index (unsigned int address);
 unsigned int get_offset (unsigned int address);
+
+unsigned int find_set(unsigned int address);
+float get_miss_rate();
+
+
 /*
 void init();
 unsigned int get_offset (unsigned int address);
-unsigned int find_set(unsigned int address);
 unsigned int select_oldest(unsigned int setnum);
 void read_tocache(unsigned int blocknum, unsigned int way, unsigned int set);
 void write_tocache(unsigned int address, unsigned char);
 unsigned char read_byte(unsigned int address);
 void write_byte(unsigned int address, unsigned char value);
-float get_miss_rate();
 */
+
+
 #endif /* __CACHE_H__ */
